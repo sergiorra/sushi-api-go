b992e4584f6badb627d4013b2eaec52f1c711d0c
Sergi Orra <sergiorra17@gmail.com>
2020-09-16 14:03:41 +0200

    sushi model

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
-
+	"encoding/json"
 )
 
+type Sushi struct {
+	ID          string `json:"id"`
+	ImageNumber string `json:"imageNumber"`
+	Name        string `json:"name"`
+	Ingredients string `json:"ingredients"`
+}
+
+var sushis []Sushi
+
 func main() {
-	
+
 }
