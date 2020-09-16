6af6beb781aefa3cf4fe26652b24e3d5f993cfd0
Sergi Orra <sergiorra17@gmail.com>
2020-09-16 15:59:42 +0200

    implementing router

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
 )
 
 type Sushi struct {
@@ -14,5 +17,15 @@ type Sushi struct {
 var sushis []Sushi
 
 func main() {
+	// initialize router
+	router := mux.NewRouter()
 
+	// endpoints
+	router.HandleFunc("/sushi", getSushis).Methods("GET")
+	router.HandleFunc("/sushi/{id}", getSushi).Methods("GET")
+	router.HandleFunc("/sushi", createSushi).Methods("POST")
+	router.HandleFunc("/sushi/{id}", updateSushi).Methods("UPDATE")
+	router.HandleFunc("/sushi/{id}", deleteSushi).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":5000", router))
 }
